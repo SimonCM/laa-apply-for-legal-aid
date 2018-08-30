591493569ab0686c09738e7ac388104142cb3a34
Simon Meredith <simoncmeredith@gmail.com>
2018-08-30 17:54:49 +0100

    Proceedings search and JS for postcode search

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const utils = require('./utils')
 
 // Route index page
 router.get('/', function (req, res) {
@@ -9,3 +10,14 @@ router.get('/', function (req, res) {
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+// Route for search proceeding
+
+router.get('/search', function (req, res) {
+
+  res.render('search',
+    {
+      proceedings: utils.getProceedings()
+    })
+
+})
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,32 +9,10 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
 
-// Mock address lookup
-  if ($('#mock-address-lookup').length) {
-    // window.loaderTime = loaderTime || 5 // globaly defined in scripts.html
-    $('.address-lookup-step2').hide()
-    $('#mock-address-lookup .js-launch-lookup').on('click', function (e) {
-      e.preventDefault()
-      $('.address-lookup-step1').hide()
+$('.address-lookup-step2').hide()
 
-
-
-      // Copy the postcode and place it into a span on the second step
-      var postcode = $('.address-lookup-step1 input').val()
-      $('.address-lookup-step2 .postcode').html(postcode)
-      return false
-    })
-    $('.change-postcode').on('click', function (e) {
-      e.preventDefault()
-      $('.address-lookup-step2').hide()
-      $('.address-lookup-step1').show()
-    })
-  }
-
-  // Loader component
-  function loadContent (loader) {
-    loader.updateMessage('5 addresses found')
-    loader.stop()
-    $('.address-lookup-step2').show()
-    // $('#event-address').focus() // needed for AT
-  }
+$('#find-address').on('click', function (e) {
+  e.preventDefault()
+  $('.address-lookup-step1').hide()
+  $('.address-lookup-step2').show()
+})
